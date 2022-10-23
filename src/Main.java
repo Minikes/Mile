173a34bb699ef40dc9872e5bf90a36c8d4169e26
Anastasia Minikes <minikes.a@mail.ru>
2022-10-24 00:33:34 +0700

    Initial

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 8012;
+        int fix = 20;
+        int mile = ticket / fix;
+
+        System.out.println(mile + " миля/миль");
+    }
+}
